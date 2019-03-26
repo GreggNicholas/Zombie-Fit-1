593e0040d11a99dc0932546fbbc5e81210287d61
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-26 16:06:30 -0400

    Create ZFitnessAdapter class

---
+++ app/src/main/java/com/example/zombiefit/Controller/ZFitnessAdapter.java
@@ -0,0 +1,30 @@
+package com.example.zombiefit.Controller;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.example.zombiefit.View.ZFitnessViewHolder;
+
+public class ZFitnessAdapter extends RecyclerView.Adapter<ZFitnessViewHolder> {
+
+    public ZFitnessAdapter() {
+    }
+    @NonNull
+    @Override
+    public ZFitnessViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int position) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ZFitnessViewHolder zFitnessViewHolder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+}
