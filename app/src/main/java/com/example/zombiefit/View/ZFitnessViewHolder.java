82a6bdbfba044bdb9a356028814308ed532db08c
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-27 20:15:55 -0400

    Edit fragment_item_cardview.xml

--- app/src/main/java/com/example/zombiefit/View/ZFitnessViewHolder.java
+++ app/src/main/java/com/example/zombiefit/View/ZFitnessViewHolder.java
@@ -1,14 +1,30 @@
 package com.example.zombiefit.View;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-public class ZFitnessViewHolder extends RecyclerView.ViewHolder {
+import com.example.zombiefit.R;
 
-    public ZFitnessViewHolder(@NonNull View itemView) {
+public class ZFitnessViewHolder extends RecyclerView.ViewHolder {
+    private CardView cardView;
+
+    public ZFitnessViewHolder(@NonNull View itemView, CardView cardView) {
         super(itemView);
+        this.cardView = cardView;
     }
 
-
+//    public void onBind(View Cardview) {
+//        cardView = itemView.findViewById(R.id.cardview_listworkoutsfragment);
+//        cardView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(cardView.getContext(), );
+//
+//            }
+//        });
+//
+//    }
 }
